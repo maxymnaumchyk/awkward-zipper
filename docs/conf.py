eb4145fce522f2b2f5d676b639024916e0590d60
maxymnaumchyk <maxymnaumchyk@gmail.com>
2025-08-01 15:37:39 +0300

    style fix

--- docs/conf.py
+++ docs/conf.py
@@ -4,11 +4,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import sys
-import os
+from pathlib import Path
 
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, Path("../src").resolve())
 print(sys.path)
 print(sys.executable)
 
