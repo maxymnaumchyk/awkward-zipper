65456c2a4ae565b8be3b0a60587f1e34c38d5fea
maxymnaumchyk <maxymnaumchyk@gmail.com>
2025-08-01 16:03:34 +0300

    try to fix sys path

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,9 @@ from pathlib import Path
 
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
-print(Path(__file__).parent)
-sys.path.append(Path(Path(__file__).parent, "..").resolve())
+sys.path.insert(0, Path("../src").resolve())
+sys.path.insert(0, Path("..").resolve())
+sys.path.insert(0, Path("../..").resolve())
 print(sys.path)
 print(sys.executable)
 
