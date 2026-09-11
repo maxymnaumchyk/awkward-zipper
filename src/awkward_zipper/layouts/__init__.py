c014439e4d352e273bf25c97c2546cb174e61f55
Iason Krommydas <iason.krom@gmail.com>
2026-09-11 21:22:47 +0200

    refactor: export every layout builder from awkward_zipper.layouts

--- src/awkward_zipper/layouts/__init__.py
+++ src/awkward_zipper/layouts/__init__.py
@@ -1,11 +1,56 @@
 from .base import BaseLayoutBuilder
+from .delphes import Delphes
+from .edm4hep import (
+    EDM4HEP,
+    EDM4HEP_v00_10_01,
+    EDM4HEP_v00_10_02,
+    EDM4HEP_v00_10_03,
+    EDM4HEP_v00_10_04,
+    EDM4HEP_v00_10_05,
+    EDM4HEP_v00_99_00,
+    EDM4HEP_v00_99_01,
+    EDM4HEP_v00_99_02,
+    EDM4HEP_v00_99_03,
+    EDM4HEP_v00_99_04,
+    EDM4HEP_v01_00,
+    EDM4HEP_v01_01,
+    edm4hep_version,
+    podio_collection_types,
+)
+from .fcc import FCC, FCCSchema, FCCSchema_edm4hep1
 from .nanoaod import NanoAOD, PFNanoAOD, ScoutingNanoAOD
+from .pdune import PDUNE
+from .physlite import PHYSLITE
+from .treemaker import TreeMaker
 
 __all__ = [
+    "EDM4HEP",
+    "FCC",
+    "PDUNE",
+    "PHYSLITE",
     "BaseLayoutBuilder",
+    "Delphes",
+    "EDM4HEP_v00_10_01",
+    "EDM4HEP_v00_10_02",
+    "EDM4HEP_v00_10_03",
+    "EDM4HEP_v00_10_04",
+    "EDM4HEP_v00_10_05",
+    "EDM4HEP_v00_99_00",
+    "EDM4HEP_v00_99_01",
+    "EDM4HEP_v00_99_02",
+    "EDM4HEP_v00_99_03",
+    "EDM4HEP_v00_99_04",
+    "EDM4HEP_v01_00",
+    "EDM4HEP_v01_01",
+    "FCCSchema",
+    "FCCSchema_edm4hep1",
     "NanoAOD",
+    "Ntuple",
     "PFNanoAOD",
     "ScoutingNanoAOD",
+    "TreeMaker",
+    "edm4hep_version",
+    "podio_collection_types",
 ]
 
 
